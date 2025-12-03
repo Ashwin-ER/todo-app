fbf20a529df3676a2f1830fcd2db9aa2b515342c
Ashwin-AIEngr <ashwin.aiengineer@gmail.com>
2025-12-04 01:18:15 +0530

    Ready for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // ADD THIS LINE (Must match your repository name with slashes)
+  base: "/todo-app/", 
 })
